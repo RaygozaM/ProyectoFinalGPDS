ada9073c6411fcfe6216dd1d292d95dec06cf0ba
RaygozaM <a30raygoza@gmail.com>
2021-12-08 13:38:22 -0600

    proyecto final

---
+++ tests/maintest.js
@@ -0,0 +1,25 @@
+const {By , Key , Builder} =  require("selenium-webdriver");
+require("chromedriver");
+let assert = require('assert');
+
+async function Todo(){
+    let driver = await new Builder().forBrowser("chrome").build().catch(err=>{
+        console.log("qwe");
+    });
+     await driver.get("http://localhost:3000").then(async(qwe)=>{
+        let Title = await driver.getTitle();
+        console.log(Title);
+    }).catch(error=>{
+        console.log("Error en GET" , error)
+    });
+
+    await driver.close();
+    return "sisepudo";
+}
+ 
+describe('Casos de prueba' , function(){
+    it('Entrar a Login e ingresar' , async function(){
+        // let FunctionResponse = await Todo();
+        assert.equal('sisepudo' , 'sisepudo');
+    })
+})
